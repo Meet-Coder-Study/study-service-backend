4ad945e11a2b107722cbecbf2019b6d2b4f197f8
SeYun <ksy90101@naver.com>
2020-08-22 14:48:27 +0900

    project init

---
+++ src/main/java/com/study/blog/StudyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.study.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyServiceApplication.class, args);
+    }
+
+}
