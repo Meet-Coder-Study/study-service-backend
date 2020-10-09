00f11d918bfec77631cd63c9f9ab6e7722fa884c
SeYun <ksy90101@naver.com>
2020-10-09 15:28:33 +0900

    feat(slack): slack message 구현

--- study-web/src/main/java/com/study/service/StudyServiceApplication.java
+++ study-web/src/main/java/com/study/service/StudyServiceApplication.java
@@ -1,16 +1,12 @@
 package com.study.service;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class StudyServiceApplication {
-    private static final String PROPERTIES = "spring.config.location="
-            + "classpath:/config/application-oauth2.yml";
 
     public static void main(final String[] args) {
-        new SpringApplicationBuilder(StudyServiceApplication.class)
-                .properties(PROPERTIES)
-                .run(args);
+        SpringApplication.run(StudyServiceApplication.class, args);
     }
 }
--- study-web/src/main/java/com/study/service/controller/IndexController.java
+++ study-web/src/main/java/com/study/service/controller/IndexController.java
@@ -1,14 +1,24 @@
 package com.study.service.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.study.service.slack.service.SlackService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 public class IndexController {
+
+    private final SlackService slackService;
+
+    @Value("${slack.token}")
+    private String token;
 
     // TODO: 2020/08/30 테스트를 위한 컨트롤러
     @GetMapping()
     public String index() {
-        return "hello";
+        return "String";
     }
 }
